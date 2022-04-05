0709df26fed12e41df0a196949be509cca636e55
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-04-05 16:18:52 +0600

    working on dashboard page

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,49 +6,43 @@ const Dashboard = () => {
 
 
     const data = [
-        {
-          name: 'Page A',
-          uv: 4000,
-          pv: 2400,
-          amt: 2400,
-        },
-        {
-          name: 'Page B',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
-        },
-        {
-          name: 'Page C',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'Page D',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
-        },
-        {
-          name: 'Page E',
-          uv: 1890,
-          pv: 4800,
-          amt: 2181,
-        },
-        {
-          name: 'Page F',
-          uv: 2390,
-          pv: 3800,
-          amt: 2500,
-        },
-        {
-          name: 'Page G',
-          uv: 3490,
-          pv: 4300,
-          amt: 2100,
-        },
-      ];
+      {
+          "month": "Mar",
+          "investment": 100000,
+          "sell": 241,
+          "revenue": 10401
+      },
+      {
+          "month": "Apr",
+          "investment": 200000,
+          "sell": 423,
+          "revenue": 24500
+      },
+      {
+          "month": "May",
+          "investment": 500000,
+          "sell": 726,
+          "revenue": 67010
+      },
+      {
+          "month": "Jun",
+          "investment": 500000,
+          "sell": 529,
+          "revenue": 40405
+      },
+      {
+          "month": "Jul",
+          "investment": 600000,
+          "sell": 601,
+          "revenue": 50900
+      },
+      {
+          "month": "Aug",
+          "investment": 700000,
+          "sell": 670,
+          "revenue": 61000
+      }
+  ]
 
       const data01 = [
         { name: 'Group A', value: 400 },
--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Error404.css'
 
 const Error404 = () => {
     return (
-        <div>
-            <h1>Error 404!</h1>
+        <div className='error-page'>
+            <img src="https://cdni.iconscout.com/illustration/premium/thumb/404-error-page-2952323-2451625.png" alt="" />
         </div>
     );
 };
